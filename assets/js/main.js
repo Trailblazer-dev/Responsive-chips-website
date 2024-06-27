40b7e406f09b6e09df4d6b8d28df57f8c2540ef0
Trailblazer-dev <richvictor830@gmail.com>
2024-06-27 11:43:12 +0300

    home complete procceeding...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,38 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu =document.getElementById('nav-menu'),
+navToggle =document.getElementById('nav-toggle'),
+navClose =document.getElementById('nav-close');
+/*Show Menu */
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu');
+    })
+}
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu'); 
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+const linkAction = ()=>{
+    const navMenu = document.getElementById('nav-menu')
+    //when we click on each nav__link , we remove the show-menu
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n=> n.addEventListener('click',linkAction));
 
 
 /*=============== SHADOW HEADER ===============*/
-
+const shadowHeader = ()=>{
+    const header = document.getElementById('header');
+    //Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >=50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
+}
+window.addEventListener('scroll', shadowHeader);
 
 /*=============== SWIPER FAVORITES ===============*/ 
 
@@ -17,4 +44,5 @@
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
 
