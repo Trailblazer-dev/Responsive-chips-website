6406f37bc7cec04d081a63aa940e9f49f5b3bc05
Trailblazer-dev <richvictor830@gmail.com>
2024-07-01 14:51:17 +0300

    finished the website

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,5 +78,16 @@ const sr = ScrollReveal({
     origin:'top',
     distance:'60px',
     duration:2500,
-    delay:300
+    delay:300,
+    reset:true,//animation set to repeat
 })
+
+sr.reveal('.home__data, .favorites__container,.footer__container')
+
+sr.reveal('.home__circle, .home__img',{delay:600,scale:.5})
+sr.reveal('.home__chips-1, .home__chips-2, .home__chips-3',{delay:1000,interval:100})
+sr.reveal('.home__leaf',{delay:1200})
+sr.reveal('.home__tomato-1, .home__tomato-2',{delay:1400,interval:100})
+sr.reveal('.care__img,.contact__img',{origin:'left'})
+sr.reveal('.care__list,.contact__data',{origin:'right'})
+sr.reveal('.banner__item,.products__card',{interval:100})
